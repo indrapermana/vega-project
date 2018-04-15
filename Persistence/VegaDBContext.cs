2cd7fc14acfcabe7b166436c155cd3068aa681c4
Indra Permana <indrapermana.jtk10@gmail.com>
2018-04-15 19:27:00 +0800

    Add migrations

---
+++ Persistence/VegaDBContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using vega.Models;
+
+namespace vega.Persistence
+{
+    public class VegaDBContext : DbContext
+    {
+        public VegaDBContext(DbContextOptions<VegaDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Make> Makes { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,16 +5,24 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using vega.Persistence;
 
 namespace vega
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -22,6 +30,8 @@ namespace vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<VegaDBContext>(options => options.UseSqlServer(Configuration["ConnectionString:Default"]));
+
             services.AddMvc();
         }
 
