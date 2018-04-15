5afd4bcc22534f6f4a4dea4a82b1859e60559680
Indra Permana <indrapermana.jtk10@gmail.com>
2018-04-15 20:08:48 +0800

    Implement autompper

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
@@ -30,6 +31,7 @@ namespace vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper();
             services.AddDbContext<VegaDBContext>(options => options.UseSqlServer(Configuration["ConnectionString:Default"]));
 
             services.AddMvc();
